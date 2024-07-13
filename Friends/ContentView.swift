aacc32f87a1e6792402261129ebce7c5685cc7fa
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-13 19:43:44 -0400

    Initial Commit

---
+++ Friends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friends
+//
+//  Created by Parth Antala on 2024-07-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Parth Antala on 2024-07-13.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
