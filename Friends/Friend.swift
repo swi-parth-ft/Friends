3c57c7db0011507c5350e5b8c2b31463bf2a469d
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-13 21:04:08 -0400

    Add custom coding keys

--- Friends/Friend.swift
+++ Friends/Friend.swift
@@ -8,8 +8,25 @@
 import Foundation
 
 struct Friend: Codable, Identifiable, Hashable {
-    let id: UUID
-    let name: String
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+    }
+    var id: UUID
+    var name: String
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+    }
+        
 }
 
 struct User: Codable, Identifiable, Hashable {
